fffbd3cead37bb583f8f947d8411c6420098f0fa
leebobae1112 <130114420+leebobae1112@users.noreply.github.com>
2023-06-27 11:43:12 +0900

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,10 +1,10 @@
 $(function () {
-  $("#design li a").click(function (e) {
+  $("#design li .poster_box a").click(function (e) {
     e.preventDefault();
-    let i = $(this).parent().index();
+    let i = $(this).parent().parent().index();
     $("#pr_popup > div").eq(i).css("display", "block");
   });
-  $("#pr_popup .close").click(function (e) {
+  $("#pr_popup a.close").click(function (e) {
     e.preventDefault();
     $("#pr_popup > div").css("display", "none");
   });
